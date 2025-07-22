b56674b4aad392091ce180328fe9528e7e7aac01
Leo <souleonardo1@gmail.com>
2025-07-22 19:25:38 -0300

    Estrutura base

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,88 @@
+#include <cstddef>
 #include <iostream>
 
 #include <ctime>
-
-#include "Solution.h"
-#include "Solver.h"
-#include "RandomSolver.cpp"
-#include "ILSSolver.h"
+#include <vector>
 #include "Data.h"
 
 using namespace std;
 
+#define SIZE dt->getDimension()
+Data * dt = NULL;
+void initializeData(char* file)
+{
+    cout << "Reading " << file << " ..." << endl;
+    static Data data = Data(2, file);
+    data.read();
+    dt = &data;
+    cout << "Succesfully read " << file << " !" << endl;
+}
+
+typedef struct Solution 
+{
+    vector<int> sequence;
+    double cost;
+    void print()
+    {
+        for(int i = 0; i < sequence.size() - 1; i++)
+            cout << sequence.at(i) << " -> ";
+        cout << sequence.back() << std::endl;
+
+        cout << "Cost: " << cost << std::endl;
+    }
+    void print(string name) {cout<<name<<"="<<endl; print(); }
+}Solution;
+void updateCost(Solution &toUpdate)
+{
+    toUpdate.cost = 0;
+    for(int i = 0; i < toUpdate.sequence.size()-1; i++) 
+        toUpdate.cost += dt->getDistance(toUpdate.sequence[i], toUpdate.sequence[i+1]);
+}
+
+/*Solution ILS(int maxIter, int maxIterIls)
+{
+    Solution bestOfAll = Solution{{},INFINITY};
+    
+    for(int i = 0; i < maxIter; i++)
+    {
+        Solution s = Construct();
+
+        Solution best = s;
+
+        int iterIls = 0;
+
+        while(iterIls <= maxIterIls)
+        {
+            LocalSearch(&s);
+            
+            if(s.cost < best.cost)
+            {
+                best = s;
+                iterIls = 0;
+            }
+
+            s = Pertubation(best);
+            
+            iterIls++;
+        }
+        if (best.cost < bestOfAll.cost)
+            bestOfAll = best;
+    }
+    return bestOfAll;
+}*/
+
 int main(int argc, char** argv) 
 {
     srand(time(NULL));
+    initializeData(argv[1]);
 
-    cout << "Reading " << argv[1] << " ..." << endl;
-
-    Data data = Data(argc, argv[1]);
-    data.read();
-
-    cout << "Succesfully read " << argv[1] << " !" << endl;
-    
     cout << "Begining ILS" << endl;
     clock_t before = clock();
-    Solver *solver = new ILSSolver(&data,
-        50,
-        data.getDimension()/(1 + (data.getDimension()>=150)));
+    Solution s = {{1,2,3,4,5,6,1}, 0};//ILS(50, SIZE/(1 + (SIZE>=150)));
+    updateCost(s);
     float duration = (clock()-before);
-    solver->solution.print((char*)"ILSSolution");
-    cout << "Concluded ILS - Took " << (float)duration/CLOCKS_PER_SEC << " seconds" << endl;
-    delete(solver);
-
-    /*solver = new RandomSolver(&data);
-    solver->solution.print((char*)"RandomSolution");
-    delete(solver);*/
+    s.print("ILS Solution");
+    cout << "Took " << (float)duration/CLOCKS_PER_SEC << " seconds" << endl;
 
     return 0;
 }
