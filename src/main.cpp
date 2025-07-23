ea430b5ace501c6da74e95061244a017efbb698e
Leo <souleonardo1@gmail.com>
2025-07-22 22:27:26 -0300

    Implementei a busca local

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 
 #include <ctime>
+#include <type_traits>
 #include <vector>
 #include <algorithm>
 #include "Data.h"
@@ -169,6 +170,187 @@ Solution Construct()
     return s;
 }
 
+bool bestImprovementSwap(Solution *s)
+{
+    double bestDelta = 0;
+    int best_i, best_j;
+
+    for(int i = 1; i < s->sequence.size() - 1; i++)
+    {
+        int vi = s->sequence[i];
+        int vi_predecessor = s->sequence[i-1];
+        int vi_succesor = s->sequence[i+1];
+
+        for(int j = i+2; j < s->sequence.size() - 1; j++)
+        {
+            int vj = s->sequence[j];
+            int vj_predecessor = s->sequence[j-1];
+            int vj_succesor = s->sequence[j+1];
+
+            double delta = 
+                -dt->getDistance(vi_predecessor, vi)
+                -dt->getDistance(vi, vi_succesor) 
+                +dt->getDistance(vi_predecessor, vj)
+                +dt->getDistance(vj, vi_succesor)
+                
+                -dt->getDistance(vj_predecessor, vj)
+                -dt->getDistance(vj, vj_succesor)
+                +dt->getDistance(vj_predecessor, vi)
+                +dt->getDistance(vi, vj_succesor);
+
+            if(delta < bestDelta)
+            {
+                bestDelta = delta;
+                best_i = i;
+                best_j = j;
+            }
+        }
+    }
+    
+    if(bestDelta < 0) 
+    {
+        std::swap(s->sequence[best_i], s->sequence[best_j]);
+        s->cost = s->cost + bestDelta;
+
+        return  true;
+    }
+    else return false;
+}
+bool bestImprovement2Opt(Solution *s)
+{
+    double bestDelta = 0;
+    int best_i, best_j;
+
+    for(int i = 0; i < s->sequence.size()-1; i++)
+    {
+        int ei_start = s->sequence[i];
+        int ei_end = s->sequence[i+1];
+
+        for(int j = i+2; j < s->sequence.size()-1; j++)
+        {
+            int ej_start = s->sequence[j];
+            int ej_end = s->sequence[j+1];
+
+            double delta = 
+                -dt->getDistance(ei_start, ei_end)
+                -dt->getDistance(ej_start, ej_end)
+                
+                +dt->getDistance(ei_start, ej_start)
+                +dt->getDistance(ei_end, ej_end);
+
+            if(delta < bestDelta)
+            {
+                bestDelta = delta;
+                best_i = i;
+                best_j = j;
+            }
+        }
+    }
+
+    if(bestDelta < 0) 
+    {
+        cout<<best_i<<" "<<best_j<<endl;
+        reverse(s->sequence.begin() + best_i + 1, s->sequence.begin() + best_j + 1);
+        s->cost = s->cost + bestDelta;
+
+        return true;
+    }
+    else return false;
+}
+bool bestImprovementOrOpt(Solution *s, int nVertex)
+{
+    double bestDelta = 0;
+    int best_block, best_edge;
+
+    for(int block_start_index = 1; block_start_index < s->sequence.size()-1 - (nVertex-1); block_start_index++)
+    {
+        int block_predecessor = s->sequence[block_start_index-1];
+        int block_start = s->sequence[block_start_index];
+        int block_end = s->sequence[block_start_index + nVertex-1];
+        int block_succesor = s->sequence[block_start_index + nVertex];
+        
+        for(int j = 0; j < s->sequence.size()-1 - (nVertex+2); j++)
+        {
+            int edge_start_index = j + (j >= block_start_index-1)*(nVertex+2);
+
+            int edge_start = s->sequence[edge_start_index];
+            int edge_end = s->sequence[edge_start_index+1];
+
+            double delta = 
+                -dt->getDistance(block_predecessor, block_start) 
+                -dt->getDistance(block_end, block_succesor)
+                
+                +dt->getDistance(block_predecessor, block_succesor)
+                -dt->getDistance(edge_start, edge_end)
+                
+                +dt->getDistance(edge_start, block_start)
+                +dt->getDistance(block_end, edge_end);
+
+
+            if(delta < bestDelta)
+            {
+                bestDelta = delta;
+                best_block = block_start_index;
+                best_edge = edge_start_index;
+            }
+        }
+    }
+
+    if(bestDelta < 0) 
+    {
+        if(best_block<best_edge)
+            rotate(
+                s->sequence.begin()+best_block, 
+                s->sequence.begin()+best_block + nVertex, 
+                s->sequence.begin()+best_edge + 1);
+        else 
+            rotate(
+                s->sequence.begin()+best_edge + 1, 
+                s->sequence.begin()+best_block, 
+                s->sequence.begin()+best_block + nVertex);
+        s->cost = s->cost + bestDelta;
+
+        return  true;
+    }
+    else return false;
+}
+
+void LocalSearch(Solution *s)
+{
+    std::vector<int> NL = {1, 2, 3, 4, 5};
+    bool improved = false;
+
+    while(!NL.empty())
+    {
+        int n = rand() % NL.size();
+
+        switch (NL[n]) 
+        {
+        case 1:
+            improved = bestImprovementSwap(s);
+            break;
+        case 2:
+            improved = bestImprovement2Opt(s);
+            break;
+        case 3:
+            improved = bestImprovementOrOpt(s,1);
+            break;
+        case 4:
+            improved = bestImprovementOrOpt(s,2);
+            break;
+        case 5:
+            improved = bestImprovementOrOpt(s,3);
+            break;
+        }
+
+        if(improved) 
+        {
+            NL = {1,2,3,4,5};
+        }
+        else NL.erase(NL.begin() + n);
+    }
+} 
+
 /*Solution ILS(int maxIter, int maxIterIls)
 {
     Solution bestOfAll = Solution{{},INFINITY};
@@ -209,6 +391,7 @@ int main(int argc, char** argv)
     cout << "Begining ILS" << endl;
     clock_t before = clock();
     Solution s = Construct();//ILS(50, SIZE/(1 + (SIZE>=150)));
+    LocalSearch(&s);
     float duration = (clock()-before);
     s.print("ILS Solution");
     cout << "Took " << (float)duration/CLOCKS_PER_SEC << " seconds" << endl;
