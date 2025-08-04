556b0f55ce4814c2a2644cf32d1ade5965423865
Leo <souleonardo1@gmail.com>
2025-08-04 18:25:42 -0300

    Implementei o BNB com uma solucao heuristica inicial

--- src/main.cpp
+++ src/main.cpp
@@ -57,6 +57,130 @@ typedef struct Solution
     void print(string name) const {cout<<name<<"="<<endl; print(); }
 }Solution;
 
+void updateCost(Solution &toUpdate)
+{
+    toUpdate.cost = 0;
+    for(int i = 0; i < toUpdate.sequence.size()-1; i++) 
+        toUpdate.cost += dt->getDistance(toUpdate.sequence[i], toUpdate.sequence[i+1]);
+}
+
+typedef struct InsertionInfo
+{
+    int insertedNode;
+    int removedEdge;
+    double cost;
+} InsertionInfo;
+vector<int> choseFromInterval(int n, int first, int limit)
+{
+    vector<int> chosen;
+    for(int i = 0; i < n; i++)
+    {
+        int toAdd = (rand() % ((limit-first)-i)) + first;
+        bool inserted = false;
+        for(int j = 0; j < chosen.size(); j++) 
+        {
+            if(toAdd < chosen[j]) 
+            {
+                chosen.insert(chosen.begin() + j,toAdd);
+                inserted = true;
+                break;
+            }
+            toAdd++;
+        }
+        if(!inserted) chosen.push_back(toAdd);
+    }
+    return chosen;
+}
+vector<int> choseRandom3NodeSequence()
+{
+    vector<int> chosen = {1};
+    vector<int> sorted3 = choseFromInterval(3, 2, dt->getDimension());
+
+    int i = rand()%3;
+    chosen.push_back(sorted3[i]);
+    sorted3.erase(sorted3.begin() + i);
+
+    i = rand()%2;
+    chosen.push_back(sorted3[i]);
+    chosen.push_back(sorted3[!i]);
+
+    chosen.push_back(1);
+    
+    return chosen;
+}
+vector<int> getUnusedNodes(const vector<int> & used)
+{
+    vector<int> unused;
+    
+    for(int i = 1; i < dt->getDimension() + 1; i++)
+    {
+        bool isUsed = false;
+        for(int node : used)
+        {
+            if(i == node) 
+            {
+                isUsed = true;
+                break;
+            }   
+        }
+        if(!isUsed) unused.push_back(i);
+    }
+
+    return unused;
+}
+InsertionInfo calculateBestInsertion
+    (const vector<int> & sequence, const vector<int> & inserting)
+{
+    InsertionInfo best;
+	best.cost = numeric_limits<double>::infinity();
+
+    for(int a = 0; a < sequence.size() - 1; a++)
+    {
+        int predecessor = sequence[a];
+        int successor = sequence[a+1];
+        for(int inserted : inserting) 
+        {
+			double cost = 
+                - dt->getDistance(predecessor-1, successor-1)
+                + dt->getDistance(predecessor-1, inserted-1)
+                + dt->getDistance(inserted-1, successor-1);
+
+			if(cost < best.cost)
+			{
+				best.cost = cost;
+				best.insertedNode = inserted;
+				best.removedEdge = a;
+			}
+        }
+    }
+    return best;
+}
+
+Solution Construct()
+{
+    Solution s = Solution();
+    
+    s.sequence = choseRandom3NodeSequence();
+    
+    updateCost(s);
+    std::vector<int> CL = getUnusedNodes(s.sequence);
+    
+    while(!CL.empty())
+    {
+        InsertionInfo insertion = 
+            calculateBestInsertion(s.sequence, CL);
+        
+        s.sequence.insert(
+            s.sequence.begin() + insertion.removedEdge + 1,
+            insertion.insertedNode);
+        s.cost += insertion.cost;
+
+        CL = getUnusedNodes(s.sequence);
+    }
+
+    return s;
+}
+
 typedef struct Node
 {
 	vector<pair<int,int>> forbidden_arcs;
@@ -171,7 +295,7 @@ _List_iterator<Node> BFSFunction(list<Node> &tree) {return tree.begin();}
 
 Solution BranchNBoundList()
 {
-	Solution best = {{}, numeric_limits<double>::infinity()};
+	Solution best = Construct();
 	double &upper_bound = best.cost; 
 
 	Node root; 
